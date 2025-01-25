df94d0aba1a971b88393db06da5c5cd1720e9c1e
Assylay <assylayshukirbay@mail.ru>
2025-01-25 11:45:58 +0500

    i have done

---
+++ src/src/main/java/models/Weather.java
@@ -0,0 +1,25 @@
+package models;
+
+public class Weather {
+    private String day;
+    private String weather;
+
+    public Weather(String day, String weather) {
+        this.day = day;
+        this.weather = weather;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public String getWeather() {
+        return weather;
+    }
+
+
+    @Override
+    public String toString() {
+        return day + ": " + weather;
+    }
+}
