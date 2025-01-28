b19a175cf64bdc50c4b804c8b2b718f6573c6ed3
White ray <white.ray.haha@gmail.com>
2025-01-28 19:49:31 +0500

    getCityById created

--- src/main/java/db/CityDatabase.java
+++ src/main/java/db/CityDatabase.java
@@ -27,3 +27,55 @@ public class CityDatabase implements ICityDatabase {
             System.out.println(e.getMessage());
         }
     }
+    @Override
+    public ArrayList<City> getAllCities() throws SQLException {
+        ArrayList <City> cities = new ArrayList<>();
+        try{
+            String sql = "Select id, place_id,city_name FROM cities";
+            PreparedStatement st = con.prepareStatement(sql);
+            ResultSet rs = st.executeQuery();
+            while(rs.next()){
+                int id = rs.getInt("id");
+                String place_id = rs.getString("place_id");
+                String city_name = rs.getString("city_name");
+                City city = new City(id,place_id,city_name);
+                cities.add(city);
+                cityCache.put(id,city);
+            }
+            st.close();
+            rs.close();
+
+            return cities;
+
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    @Override
+    public String getCityById(int id){
+        try{
+            String sql = "Select id,place_id,city_name FROM cities WHERE id=?";
+            PreparedStatement st = con.prepareStatement(sql);
+            st.setInt(1,id);
+            ResultSet rs = st.executeQuery();
+            if(rs.next()){
+                String place_id = rs.getString("place_id");
+                String city_name = rs.getString("city_name");
+                City city = new City(id,place_id,city_name);
+                cityCache.put(id,city);
+
+                return place_id;
+
+            }else{
+                throw new CityNotFoundException("City with Id " + id + " not found.");
+            }
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        } catch (CityNotFoundException e) {
+            System.out.println(e.getMessage());;
+        }
+        return null;
+    }
+}
