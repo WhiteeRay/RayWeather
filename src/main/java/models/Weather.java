d4ed97de320036dde4ecab057ea127b4e9151f7c
Assylay <assylayshukirbay@mail.ru>
2025-01-28 20:56:05 +0500

    constructor has been created

--- src/main/java/models/Weather.java
+++ src/main/java/models/Weather.java
@@ -3,4 +3,9 @@ package models;
 public class Weather {
     private String day;
     private String weather;
+
+    public Weather(String day, String weather) {
+        this.day = day;
+        this.weather = weather;
+    }
 }
