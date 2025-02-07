d496e367e674779b0065ddb882dde9fbf0f2aedf
White ray <white.ray.haha@gmail.com>
2025-02-07 18:50:21 +0500

    updated

--- src/main/java/org/example/rayweather/models/Weather.java
+++ src/main/java/org/example/rayweather/models/Weather.java
@@ -1,37 +1,15 @@
-public class bodypart {public class Weather {
+package org.example.rayweather.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Weather {
     private String day;
     private String weather;
 
-
-    public Weather(String day, String weather) {
-        this.day = day;
-        this.weather = weather;
-    }
-
-
-    public String getDay() {
-        return day;
-    }
-
-
-    public void setDay(String day) {
-        this.day = day;
-    }
-
-
-    public String getWeather() {
-        return weather;
-    }
-
-
-    public void setWeather(String weather) {
-        this.weather = weather;
-    }
-
-
-    public void displayWeather() {
-        System.out.println("Day: " + day + ", Weather: " + weather);
-    }
-}
 
 }
