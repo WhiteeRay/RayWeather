e14dd302f8b3fbdea1c211fe51b1c3f405470d7a
Lenovo <usmanovehsonjon@gmail.com>
2025-02-07 18:16:51 +0500

    body

--- src/main/java/org/example/rayweather/models/Weather.java
+++ src/main/java/org/example/rayweather/models/Weather.java
@@ -1,24 +1,37 @@
-package org.example.rayweather.models;
-
-public class Weather {
+public class bodypart {public class Weather {
     private String day;
     private String weather;
+
 
     public Weather(String day, String weather) {
         this.day = day;
         this.weather = weather;
     }
 
+
     public String getDay() {
         return day;
     }
+
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
 
     public String getWeather() {
         return weather;
     }
 
-    @Override
-    public String toString() {
-        return day + ": " + weather;
+
+    public void setWeather(String weather) {
+        this.weather = weather;
     }
+
+
+    public void displayWeather() {
+        System.out.println("Day: " + day + ", Weather: " + weather);
+    }
+}
+
 }
