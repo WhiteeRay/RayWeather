9e09fd338f48427a509f22a53881bde8f58c5dd2
White ray <white.ray.haha@gmail.com>
2025-02-07 19:33:36 +0500

    updated

--- src/main/java/org/example/rayweather/configurations/WeatherConfig.java
+++ src/main/java/org/example/rayweather/configurations/WeatherConfig.java
@@ -1,4 +1,4 @@
-package org.example.springbootdemoversion.configurations;
+package org.example.rayweather.configurations;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
