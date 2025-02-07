059206e58bd0678195916ff505118477104bfa5b
White ray <white.ray.haha@gmail.com>
2025-02-07 19:05:35 +0500

    updated

--- src/main/java/org/example/rayweather/dto/CityDto.java
+++ src/main/java/org/example/rayweather/dto/CityDto.java
@@ -1,4 +1,4 @@
-package dto;
+package org.example.rayweather.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
--- src/main/java/org/example/rayweather/entities/Weather.java
+++ src/main/java/org/example/rayweather/entities/Weather.java
@@ -1,5 +1,4 @@
 package org.example.rayweather.entities;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
