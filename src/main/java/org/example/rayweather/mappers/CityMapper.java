0bef43a135d103acb6f48900ed548e5d92909010
White ray <white.ray.haha@gmail.com>
2025-02-13 09:24:03 +0500

    updated

--- src/main/java/org/example/rayweather/mappers/CityMapper.java
+++ src/main/java/org/example/rayweather/mappers/CityMapper.java
@@ -3,18 +3,11 @@ import org.example.rayweather.dto.CityDto;
 import org.example.rayweather.entities.City;
 
 public class CityMapper {
-    public static CityDto mapToCityDto(City city){
+    public static CityDto mapToCityDto(City city) {
         return new CityDto(
                 city.getId(),
                 city.getPlace_id(),
                 city.getCity_name()
-        );
-    }
-    public static City mapToCity(CityDto cityDto){
-        return new City(
-                cityDto.getId(),
-                cityDto.getPlace_id(),
-                cityDto.getCity_name()
         );
     }
 }
